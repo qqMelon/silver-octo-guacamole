d2e2411062959cce3407fe707d82980ac5b3de4d
qqMelon <urie.zahard@gmail.com>
2023-01-06 08:26:26 +0100

    Write unzip function and it worked

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -71,6 +73,40 @@ func checkRemoteVersion() string {
 	return rv
 }
 
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	remoteVersion := checkRemoteVersion()
 	fmt.Println(remoteVersion)
@@ -79,5 +115,6 @@ func main() {
 		fmt.Println("You already have the latest version of ElvUI")
 		return
 	}
-	downloadPackage(version)
+	downloadPackage(remoteVersion)
+	Unzip("elvui-"+remoteVersion+".zip", "Addons/")
 }
