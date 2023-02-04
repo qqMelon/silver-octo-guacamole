8c52c8be893aab9c5589d97806646d7e30c7d1a1
qqMelon <urie.zahard@gmail.com>
2023-02-04 14:18:18 +0100

    create interactive shell

--- addonmanager.go
+++ addonmanager.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-func main() {
-	dirs, err := ioutil.ReadDir("./../AddOns/")
+func testInteractiveShell() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		err = runCommand(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func runCommand(commandStr string) error {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
+		// add another case here for custom commands.
+	}
+	cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
+func dirReader() {
+	dirs, err := ioutil.ReadDir("./AddOns/")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,4 +49,8 @@ func main() {
 	for _, f := range dirs {
 		fmt.Println(f.Name())
 	}
+}
+
+func main() {
+	testInteractiveShell()
 }
