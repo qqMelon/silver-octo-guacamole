1754f2f611d1835009526d6b61df25d2d6fc8fd9
qqMelon <urie.zahard@gmail.com>
2023-02-07 00:44:10 +0100

    delete comment

--- main.go
+++ main.go
@@ -3,22 +3,5 @@ package main
 import "github.com/qqMelon/silver-octo-guacamole/app"
 
 func main() {
-	//remoteVersion := script.CheckRemoteVersion()
-	//fmt.Println("Remote version: ", remoteVersion)
-
 	app.Window()
-
-	//if script.CheckLocalVersion(remoteVersion) {
-	//	fmt.Println("You already have the latest version of ElvUI")
-	//	time.Sleep(3 * time.Second)
-	//	return
-	//}
-
-	//fmt.Println("Downloading ElvUI...")
-	//script.DownloadPackage(remoteVersion)
-	//fmt.Println("Download complete")
-	//fmt.Println("Extracting...")
-	//script.Unzip("elvui-"+remoteVersion+".zip", "Addons/")
-	//fmt.Println("Extracting done at : Addons/")
-	//time.Sleep(3 * time.Second)
 }
