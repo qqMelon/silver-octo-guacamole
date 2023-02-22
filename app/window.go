00a8f5b41aebe4e1083921c64d17bf2f40283878
Maxime NICOLAS <maxime.nicolas@conserto.pro>
2023-02-22 10:27:48 +0100

    Create gui

--- app/window.go
+++ app/window.go
@@ -3,29 +3,58 @@ package app
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/qqMelon/silver-octo-guacamole/script"
 )
 
 func Window() {
-	a := app.New()
-	w := a.NewWindow("Silver Octo Guacamol")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Silver Octo Guacamole")
 
-	desc := widget.NewLabel("Update, Install, manage your addons with SOG")
+	description := widget.NewLabel("Update, install, manage your World Of Warcraft addons with SOG !")
 
-	elvuiBtn := Button("Update/Install ElvUI", "updateElvui")
-	updateBtn := Button("Speed update", "speedUpdate")
-	manageBtn := Button("Manage addons", "manageAddon")
+	str := binding.NewString()
 
-	btnContainer := container.New(layout.NewHBoxLayout(), elvuiBtn, updateBtn, manageBtn)
-	desContainer := container.New(layout.NewHBoxLayout(), desc)
+	// Buttons
+	elvuiBtn := widget.NewButton("Update/Install ElvUI", func() {
+		err := str.Set("Update/Install ElvUI processing ...")
+		if err != nil {
+			return
+		}
+		err, resultVal := script.Elvui()
+		err = str.Set(resultVal)
+		if err != nil {
+			return
+		}
+	})
+	speedUpdateBtn := widget.NewButton("Speed Update", func() {
+		err := str.Set("Start listing all addons ...")
+		if err != nil {
+			return
+		}
+		script.Update()
+		str.Set("Games retrieved")
+	})
+	manageBtn := widget.NewButton("Manage Addons", func() {
+		// WIP
+		err := str.Set("Not available atm, on working ...")
+		if err != nil {
+			return
+		}
+	})
 
-	//mainContent := container.New(layout.NewVBoxLayout(), desc, content, ActionStatus())
+	// Dynamic data
+	dynamicStr := widget.NewLabelWithData(str)
 
-	centeredBtnContainer := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), btnContainer, layout.NewSpacer())
-	centeredDescContainer := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), desContainer, layout.NewSpacer())
+	// Container
+	btnContainerCentered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), elvuiBtn, speedUpdateBtn, manageBtn, layout.NewSpacer())
+	descriptionCentered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), description, layout.NewSpacer())
+	dynamicStrCentered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), dynamicStr, layout.NewSpacer())
 
-	w.SetContent(container.New(layout.NewVBoxLayout(), centeredDescContainer, centeredBtnContainer))
+	mainContent := container.New(layout.NewVBoxLayout(), descriptionCentered, dynamicStrCentered, btnContainerCentered)
 
-	w.ShowAndRun()
+	myWindow.SetContent(mainContent)
+	myWindow.ShowAndRun()
 }
